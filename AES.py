aec7892467c98838a23e0cb7a8be9be7b5d60523
jokecake <jokecake109@gmail.com>
2026-04-15 11:59:41 -0300

    Update AES.py

--- AES.py
+++ AES.py
@@ -4,11 +4,10 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 
 arquivo = sys.argv[1]
-
+chave = sys.argv[2]
 with open(arquivo) as f:
     arquivo = base64.b64decode(f.read())
 
-chave = b"YELLOW SUBMARINE"
 cipher = AES.new(chave, AES.MODE_ECB)
 
 descriptografado = unpad(cipher.decrypt(arquivo),16)
