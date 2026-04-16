a06f9924db83f86fa34f231ddeaad5caea2e2acd
jokecake <jokecake109@gmail.com>
2026-04-15 23:00:11 -0300

    Update AES.py

--- AES.py
+++ AES.py
@@ -1,15 +1,22 @@
 import base64
 import sys
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
+from Crypto.Util.Padding import unpad, pad
 
-arquivo = sys.argv[1]
-chave = sys.argv[2]
-with open(arquivo) as f:
-    arquivo = base64.b64decode(f.read())
+modo = sys.argv[1]
+arquivo = sys.argv[2]
+chave = sys.argv[3].encode()
+
+with open(arquivo, "rb") as f:
+    data = f.read()
 
 cipher = AES.new(chave, AES.MODE_ECB)
 
-descriptografado = unpad(cipher.decrypt(arquivo),16)
+if modo == "dec":
+    data = base64.b64decode(data)
+    resultado = unpad(cipher.decrypt(data), 16)
+    print(resultado.decode("utf-8"))
 
-print(descriptografado.decode("utf-8"))
+elif modo == "enc":
+    criptografado = cipher.encrypt(pad(data, 16))
+    print(base64.b64encode(criptografado).decode())
