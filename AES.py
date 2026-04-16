b55541504551290d6be18c3e2c954f954dcc4e88
jokecake <jokecake109@gmail.com>
2026-04-16 12:23:25 -0300

    Enhance AES script with key input and file output
    Added input handling for encryption key and file output for results.

--- AES.py
+++ AES.py
@@ -3,9 +3,20 @@ import sys
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad, pad
 
+if len(sys.argv) < 3:
+    print("Uso: python AES.py <enc/dec> <arquivo> [chave]")
+    sys.exit()
+
 modo = sys.argv[1]
 arquivo = sys.argv[2]
-chave = sys.argv[3].encode()
+
+if len(sys.argv) >= 4:
+    chave = sys.argv[3].encode()
+else:
+    chave = input("Digite a chave: ").encode()
+
+
+chave = chave.ljust(16, b'\0')[:16]
 
 with open(arquivo, "rb") as f:
     data = f.read()
@@ -13,10 +24,25 @@ with open(arquivo, "rb") as f:
 cipher = AES.new(chave, AES.MODE_ECB)
 
 if modo == "dec":
-    data = base64.b64decode(data)
-    resultado = unpad(cipher.decrypt(data), 16)
-    print(resultado.decode("utf-8"))
+    try:
+        data = base64.b64decode(data)
+        resultado = unpad(cipher.decrypt(data), 16)
+
+        
+        with open(arquivo, "wb") as f:
+            f.write(resultado)
+
+        print("arquivo descriptografado com sucesso!")
+
+    except:
+        print("erro: chave errada ou arquivo corrompido")
+
 
 elif modo == "enc":
     criptografado = cipher.encrypt(pad(data, 16))
-    print(base64.b64encode(criptografado).decode())
+    b64 = base64.b64encode(criptografado)
+
+    with open(arquivo, "wb") as f:
+        f.write(b64)
+
+    print("Arquivo criptografado com sucesso!")
